d7158b590d4ff0cedaa199cf7fcb22d95058a33d
gos1004 <nnv711@gmail.com>
2021-09-05 12:47:13 +0900

    todoApp 구현-리스트,삭제,저장

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        header
+        <h1>TODO it!</h1>
     </div>
 </template>
 
@@ -10,6 +10,10 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,76 @@
 <template>
     <div>
-        list
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" v-on:click="toggleComplete"></i>
+                {{ todoItem }}
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-   
+    data: function(){
+        return {
+            todoItems : []
+        }
+    },
+    methods: {
+        removeTodo: function(todoItem, index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        },
+        toggleComplete: function(){
+
+        }
+    },
+    created: function(){
+      if(localStorage.length > 0){
+          for(var i = 0; i < localStorage.length; i ++){
+              if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                this.todoItems.push(localStorage.key(i));
+              }
+          }
+      }
+   }
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li {
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .removeBtn {
+        margin-left: auto;
+        color: #de4343;
+    }
+    .checkBtn {
+        line-height: 50px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .checkBtnCompleted {
+        color: #b3adad;
+    }
+    .textCompleted {
+        text-decoration: line-through;
+        color: #b3adad;
+    }
 </style>
