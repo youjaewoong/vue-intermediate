c22f1ff440925da6f22ae86018a642a11b1ab445
gos1004 <nnv711@gmail.com>
2021-09-05 22:32:38 +0900

    [리팩토링] 할 일 추가 기능 emit으로 하위컴포넌트에서 상위컴포넌트에 데이터 전달

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
@@ -18,6 +18,14 @@ export default {
   data: function() {
     return {
       todoItems: []
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem){
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      //목록 갱신
+      this.todoItems.push(obj)
     }
   },
   created: function(){
