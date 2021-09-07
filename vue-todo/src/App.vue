75cedd2cea6cfb18400e3ae8d8370c84d958915c
gos1004 <nnv711@gmail.com>
2021-09-07 23:23:36 +0900

    Enhanced Object Literals - 향상된 객체 리터럴

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -18,34 +18,34 @@ import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 
 export default {
-  data: function() {
+  data(){
     return {
       todoItems: []
     }
   },
   methods: {
-    addOneItem: function(todoItem){
+    addOneItem(todoItem){
       const obj = {completed: false, item: todoItem};
       localStorage.setItem(todoItem, JSON.stringify(obj));
       //목록 갱신
       this.todoItems.push(obj)
     },
-    removeOneItem: function(todoItem, index){
+    removeOneItem(todoItem, index){
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index){
+    toggleOneItem(todoItem, index){
       this.todoItems[index].completed = !this.todoItems[index].completed;
       // 로컬 스토리지의 데이터를 갱신
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem, JSON.stringify(todoItem));
     },
-    clearAllItems: function(){
+    clearAllItems(){
       localStorage.clear();
       this.todoItems = [];
     }
   },
-  created: function(){
+  created(){
   if(localStorage.length > 0){
       for(let i = 0; i < localStorage.length; i ++){
           if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
@@ -55,10 +55,10 @@ export default {
     }
   },
   components: {
-    'TodoHeader' : TodoHeader,
-    'TodoInput' : TodoInput,
-    'TodoList' : TodoList,
-    'TodoFooter' : TodoFooter
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
   }
 }
 </script>
