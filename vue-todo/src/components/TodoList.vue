f1053e8f3fc22ac58ad64eeaf5e329508881fe12
gos1004 <nnv711@gmail.com>
2021-09-11 23:33:19 +0900

    [리팩토링] Todo 추가,삭제,전체삭제, 할일완료 store : state mutations : commit

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -17,13 +17,12 @@
 
 <script>
 export default {
-    props: ['propsdata'],
     methods: {
         removeTodo(todoItem, index){
-            this.$emit('removeItem', todoItem, index);
+            this.$store.commit('removeOneItem', {todoItem, index});
         },
         toggleComplete(todoItem, index){
-            this.$emit('toggleItem', todoItem, index);
+            this.$store.commit('toggleOneItem', {todoItem, index});
         }
     }
 };
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -21,5 +21,25 @@ const storage = {
 export const store = new Vuex.Store({
     state: {
         todoItems: storage.fetch()
+    },
+    mutations: {
+        addOneItem(state, todoItem) {
+            const obj = {completed: false, item: todoItem};
+            localStorage.setItem(todoItem, JSON.stringify(obj));
+            state.todoItems.push(obj);
+        },
+        removeOneItem(state, payload){
+            localStorage.removeItem(payload.todoItem.item);
+            state.todoItems.splice(payload.index, 1);
+        },
+        toggleOneItem(state, payload){
+            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+            localStorage.removeItem(payload.todoItem.item);
+            localStorage.setItem(payload.todoItem, JSON.stringify(payload.todoItem));
+        },
+        clearAllItems(state){
+            localStorage.clear();
+            state.todoItems = [];
+        }
     }
 })
