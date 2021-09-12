f31586ae985bff4c2337140d5b8b84f7e2c2d628
gos1004 <nnv711@gmail.com>
2021-09-12 13:48:12 +0900

    스토어 모듈화 방법

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,28 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './geeters'
-import * as mutations  from './mutations'
+import todoApp from './modules/todoApp'
+// import * as getters from './geeters'
+// import * as mutations  from './mutations'
 
 //global영역에 플러그인을 설치하여 사용하는 코드
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if(localStorage.length > 0){
-            for(let i = 0; i < localStorage.length; i ++){
-                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    }
-};
 export const store = new Vuex.Store({
-    state: {
-        todoItems: storage.fetch()
-    },
-    getters,
-    mutations
+    modules: {
+        todoApp
+    }
 })
